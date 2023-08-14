c0032ccd30cb3739919e125903da415345225c76
GLOGLOGLO3355 <rodrigomurga@gmail.com>
2023-08-14 22:15:36 +0200

    animWalk
    Player movement

---
+++ 2D TopDown RPG test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Obtener la entrada horizontal y vertical
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calcular el vector de movimiento
+        rb.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
+    }
+}
+
