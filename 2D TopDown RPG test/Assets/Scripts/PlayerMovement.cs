51896cc079b489c8d3a60ef9c74e58e5c93d0d3a
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2023-08-15 01:27:17 +0200

    NUIM¡
    >

--- 2D TopDown RPG test/Assets/Scripts/PlayerMovement.cs
+++ 2D TopDown RPG test/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
     private void Update()
     {
         // Obtener la entrada horizontal y vertical
@@ -20,6 +21,34 @@ public class PlayerMovement : MonoBehaviour
 
         // Calcular el vector de movimiento
         rb.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
+        if (horizontalInput < 0){
+            transform.rotation = Quaternion.Euler(0f,180f,0f);
+        }
+        if (horizontalInput > 0){
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        if (horizontalInput != 0) {
+            anim.SetBool("isWalkingSide", true);
+            anim.SetBool("isWalkingUp", false);
+            anim.SetBool("isWalkingDown", false);
+        }
+        else {
+            if (verticalInput<0){
+                anim.SetBool("isWalkingUp", false);
+                anim.SetBool("isWalkingDown", true);
+            }
+            else if (verticalInput > 0){
+                anim.SetBool("isWalkingDown", false);
+                anim.SetBool("isWalkingUp", true);
+            }
+            else
+            {
+                anim.SetBool("isWalkingSide", false);
+                anim.SetBool("isWalkingUp", false);
+                anim.SetBool("isWalkingDown", false);
+            }
+        }
+
     }
 }
 
