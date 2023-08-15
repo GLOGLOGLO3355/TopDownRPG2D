c10187949c80db6787c3731bd8d30946bfa15fa3
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2023-08-15 23:40:15 +0200

    Camara y arbol
    Camara sigue al jugador y arbol rojo funciona

---
+++ 2D TopDown RPG test/Assets/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public PlayerMovement playerMovement;
+    public float Algo;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(true)
+        {
+            Vector3 desiredPosition = player.position + offset;
+            //desiredPosition.y = transform.position.y;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position,desiredPosition,Algo);
+            transform.position = smoothedPosition;
+        }
+    }
+}
